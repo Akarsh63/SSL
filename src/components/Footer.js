8a169ed4a123224895a5339eea82f167f7fc38c8
Akarsh63 <akarsh.1@iitj.ac.in>
2024-03-19 19:26:18 +0530

    updated logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <Box sx={{padding:{sm:'25px 70px',xs:'10px'},border:'1px solid #000000',margin:'0 5px',display:'flex',flexDirection:'row',gap:'10px',alignItems:'center',justifyContent:'space-between',flexWrap:'wrap',marginBottom:'10px'}}>
        <Box>
-          <Typography sx={{fontSize:'25px',fontWeight:'400'}}>SSI Colloab</Typography>
+          <Typography sx={{fontSize:'25px',fontWeight:'400'}}>Zentry</Typography>
        </Box>
        <Box>
           <Typography sx={{fontSize:'15px',fontWeight:'400'}}>Version 2.3.01</Typography>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaUser } from "react-icons/fa";
+import zentry from '../images/zentry_logo.webp';
 
 export default function Header() {
   return (
     <Box sx={{padding:'0 10px'}}>
           <Box sx={{display:'flex',flexDirection:'row',justifyContent:'space-between',padding:'5px'}}>
               <Box sx={{display:'flex',flexDirection:'row',alignItems:'center'}}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGVOJM5XNbHJLkMqjOQwBPxoZdM2R_gA9wO2sR9ezERU-BMhIM9pLlfy-MHfy-GxWC3ic&usqp=CAU" alt="Logo" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-                    <Typography sx={{fontSize:'20px',fontWeight:'700'}}>SSI Colloab</Typography>
+                    <img src={zentry} alt="Logo" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+                    <Typography sx={{fontSize:'20px',fontWeight:'700'}}>Zentry</Typography>
               </Box>
               <Box >
                 <Button startIcon={<FaUser />} sx={{color:'gold',textTransform:'none',backgroundColor:'transparent'}} >test1@test.com</Button>
